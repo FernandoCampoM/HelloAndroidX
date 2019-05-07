ac8a9ea1a847a974d03478872c0c4f66d34614f8
Fernando Campo <juancamm@unicauca.edu.co>
2019-05-07 16:33:12 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/fernandocampo/helloandroidx/clsHelloAndroidX.java
@@ -0,0 +1,30 @@
+package com.example.fernandocampo.helloandroidx;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import java.util.Date;
+
+import android.view.View;
+import android.widget.Button;
+
+public class clsHelloAndroidX extends AppCompatActivity implements View.OnClickListener {
+
+     Button atrButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_andorid_x);
+        atrButton = (Button) findViewById(R.id.atrButton);
+        atrButton.setOnClickListener(this);
+        UpDateTime();
+    }
+    public void UpDateTime(){
+        atrButton.setText(new Date().toString());
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        UpDateTime();
+    }
+}
